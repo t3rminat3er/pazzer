0a2b4758fa0a636a195ffa82002aa86a072cb9c4
WoelliJ <johannes.woelfl@stud.uni-regensburg.de>
2015-07-29 21:31:56 +0200

    fix for sidedeckservice
    cannot produce 0 cards - can produce +6

--- js/services/sideDeckService.js
+++ js/services/sideDeckService.js
@@ -2,8 +2,12 @@
     random: function () {
         var cards = [];
         for (var i = 0; i < 4; i++) {
-            var value = Math.floor(Math.random() * 12);
+            var value = Math.floor(Math.random() * 13) ;
             value = value - 6;
+            if (value === 0) {
+                i--;
+                continue;
+            }
             cards.push(new App.Card(value));
         }
         console.log("random side deck", cards);
